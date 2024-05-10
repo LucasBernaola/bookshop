60888336d20528f4c700da12f0b301d6ade62e5d
Berentolkien <bernalb2014@gmail.com>
2024-05-09 23:07:46 -0300

    fix db

--- Backend/bookshop/settings.py
+++ Backend/bookshop/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 if RENDER_EXTERNAL_HOSTNAME:
-      ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite://db.sqlite3',
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600
     )
 }
